758a5a9d88b1dccb7a9bd144733e168dc2fe196a
NaveenVinayan <vinayannaveen@gmail.com>
2025-09-15 10:23:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import AdminDashboard from './pages/AdminDashboard.jsx'
+import EmployeeDashboard from './pages/EmployeeDashboard.jsx'
+import PrivateRoutes from './utils/PrivateRoutes.jsx'
+import RoleBasedRoutes from './utils/RoleBasedRoutes.jsx'
+import AdminSummary from './components/dashboard/AdminSummary.jsx'
+import DepartmentList from './components/department/DepartmentList.jsx'
+import AddDepartment from './components/department/AddDepartment.jsx'
+import EditDepartment from './components/department/EditDepartment.jsx'
+import List from './components/employee/List.jsx'
+import Add from './components/employee/Add.jsx'
+import View from './components/employee/View.jsx'
+import Edit from './components/employee/Edit.jsx'
+import AddSalary from './components/salary/Add.jsx'
+import ViewSalary from './components/salary/View.jsx'
+import Summary from './components/EmployeeDashboard/Summary.jsx'
+import LeaveList from './components/leave/List.jsx'
+import AddLeave from './components/leave/Add.jsx'
+import Setting from './components/EmployeeDashboard/Setting.jsx'
+import Table from './components/leave/Table.jsx'
+import Detail from './components/leave/Detail.jsx'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to="/admin-dashboard" />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/admin-dashboard' element={
+          <PrivateRoutes>
+            <RoleBasedRoutes requiredRole={["admin"]}>
+              <AdminDashboard />
+            </RoleBasedRoutes>
+          </PrivateRoutes>
+
+        }>
+          <Route index element={<AdminSummary />}></Route>
+
+          <Route path='/admin-dashboard/departments' element={<DepartmentList />}></Route>
+          <Route path='/admin-dashboard/add-department' element={<AddDepartment />}></Route>
+          <Route path='/admin-dashboard/department/:id' element={<EditDepartment />}></Route>
+
+
+          <Route path='/admin-dashboard/employees' element={<List />}></Route>
+          <Route path='/admin-dashboard/add-employee' element={<Add />}></Route>
+          <Route path='/admin-dashboard/employees/:id' element={<View />}></Route>
+          <Route path='/admin-dashboard/employees/edit/:id' element={<Edit />}></Route>
+          <Route path='/admin-dashboard/employees/salary/:id' element={<ViewSalary />}></Route>
+
+          <Route path='/admin-dashboard/salary/add' element={<AddSalary />}></Route>
+
+          <Route path='/admin-dashboard/Leaves' element={<Table />}></Route>
+          <Route path='/admin-dashboard/Leaves/:id' element={<Detail />}></Route>
+          <Route path='/admin-dashboard/employees/Leaves/:id' element={<LeaveList />}></Route>
+
+          <Route path='/admin-dashboard/setting' element={<Setting />}></Route>
+
+
+
+
+        </Route>
+        <Route path='/employee-dashboard' element={
+          <PrivateRoutes>
+            <RoleBasedRoutes requiredRole={["admin", "employee"]}>
+              <EmployeeDashboard />
+            </RoleBasedRoutes>
+          </PrivateRoutes>
+
+        }>
+
+          <Route index element={<Summary />}></Route>
+          <Route path='/employee-dashboard/profile/:id' element={<View />}></Route>
+          <Route path='/employee-dashboard/leaves/:id' element={<LeaveList />}></Route>
+          <Route path='/employee-dashboard/add-leave' element={<AddLeave />}></Route>
+          <Route path='/employee-dashboard/salary/:id' element={<ViewSalary />}></Route>
+          <Route path='/employee-dashboard/setting' element={<Setting />}></Route>
+
+
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/dashboard/AdminSummary.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import SummaryCard from './SummaryCard'
+import { FaBuilding, FaCheckCircle, FaFileAlt, FaHourglassHalf, FaMoneyBillWave, FaTimesCircle, FaUsers } from 'react-icons/fa'
+import axios from 'axios'
+const AdminSummary = () => {
+    const [summary, setSummary] = useState(null)
+
+    useEffect(() => { 
+        const fetchSummary = async () => {
+            try {
+                const summary = await axios.get('http://localhost:5000/api/dashboard/summary', {
+                    headers: {
+                        "Authorization": `Bearer ${localStorage.getItem('token')}`
+                    }
+                })
+                setSummary(summary.data)
+                
+            } catch (error) {
+                if (error.response) {
+                    alert(error.response.data.error)
+                }
+                console.log(error.message);
+                
+
+            }
+        }
+        fetchSummary()
+    }, [])
+
+    if (!summary) {
+        return <div>Loading....</div>
+    }
+
+    return (
+        <div className='p-6'>
+            <h3 className='text-2xl font-bold'>Dashboard Overview</h3>
+            <div className=' grid grid-cols-1 md:grid-cols-3 gap-4 mt-6'>
+                <SummaryCard icon={<FaUsers />} text="Total Employees" number={summary.totalEmployees} color="bg-teal-600" />
+                <SummaryCard icon={<FaBuilding />} text="Total Departments" number={summary.totalDepartment} color="bg-yellow-600" />
+                <SummaryCard icon={<FaMoneyBillWave />} text="Monthly Salary" number={summary.totalSalaries} color="bg-red-600" />
+            </div>
+
+            <div className='mt-12'>
+                <h4 className='text-center text-2xl font-bold'>Leave details</h4>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-6'>
+                    <SummaryCard icon={<FaFileAlt />} text="Leave Applied" number={summary.leaveSummary.appliedFor} color="bg-teal-600" />
+                    <SummaryCard icon={<FaCheckCircle />} text="Leave Approved" number={summary.leaveSummary.approved} color="bg-green-600" />
+                    <SummaryCard icon={<FaHourglassHalf />} text="Leave pending" number={summary.leaveSummary.Pending} color="bg-yellow-600" />
+                    <SummaryCard icon={<FaTimesCircle />} text="Leave Rejected" number={summary.leaveSummary.Rejected} color="bg-red-600" /> 
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AdminSummary
